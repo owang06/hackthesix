42b0b35fb65e79811afed2213f27982e75b4620c
Samuel Liao <samliao6@student.ubc.ca>
2025-07-19 11:15:32 -0400

    Improved object detection through twelvelabs api and added convertion of response to .txt file

--- backend/twelve_labs.py
+++ backend/twelve_labs.py
@@ -1,4 +1,5 @@
 import os
+import re
 import uuid
 from dotenv import load_dotenv
 from twelvelabs import TwelveLabs
@@ -13,7 +14,7 @@ client = TwelveLabs(api_key=API_KEY)
 
 INDEX_NAME = str(uuid.uuid4())
 
-VIDEO_PATH = "tempvideos/IMG_1756.MP4"
+VIDEO_PATH = "tempvideos/VID20250719105124.mp4"
 
 # 2. Create an index
 models = [
@@ -44,8 +45,39 @@ print(f"The unique identifier of your video is {task.video_id}.")
 # 5. Perform open-ended analysis
 text = client.analyze(
   video_id=task.video_id,  
-  prompt="Identify the important timestamp(s) -- 1 or more -- containing key details such as a new set of objects",
+  prompt="""
+  1. Tell me EVERY SINGLE furniture object (eg table, chair, computer, dresser, bed, etc) 
+  that you see in this video, along with how many of each you see. 
+  2. Give me a timestamp for each object where at least one of the objects is fully visible in frame
+  3. **Important** Make sure to get every object possible regardless of how small or simple.
+  4. **Important** Tell me how many items there are total of each object.
+  5. Provide a response in a similar style as this template 
+  but with temp replaced with the respective objects found, # replaced with their respective timestamps, 
+  and 'n' replaced with the respective number of each object found. 
+  Also extending or truncating the template for however many items there are:
+
+  In the video, the following furniture objects are visible:
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    temp: n - Visible at _____ [#s (##:##)].
+    ...
+    ...
+    ...
+  6. **IMPORTANT** I ONLY WANT 1 TIMESTAMP FOR EACH OBJECT.
+  7. **IMPORTANT** MAKE SURE THAT THE OBJECTS ARE VISIBLE AT THE TIMESTAMP PROVIDED.
+  """,
   # temperature=0.2
 )
 # 6. Process the results
 print(f"{text.data}")
+
+matches = re.findall(r"- (\w[\w\- ]*):.*(\[\d+s \(\d+:\d+\)\])", text.data)
+
+with open("output.txt", "w") as f:
+    for obj, timestamp in matches:
+        f.write(f"{obj} {timestamp}\n")
